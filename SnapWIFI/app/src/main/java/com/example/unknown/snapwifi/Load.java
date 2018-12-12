d09ddf8e57773510e7d1b6203197ebcf486263dd
BSAA0203 <33346331+BSAA0203@users.noreply.github.com>
2018-12-13 01:38:05 +0900

    FIX

---
+++ SnapWIFI/app/src/main/java/com/example/unknown/snapwifi/Load.java
@@ -0,0 +1,275 @@
+package com.example.unknown.snapwifi;
+
+
+
+import android.app.ProgressDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.example.unknown.snapwifi.Cam.RT;
+
+/*import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;*/
+
+
+
+/*
+    <com.google.android.gms.ads.AdView
+        android:id="@+id/lv"
+        ads:adSize="BANNER"
+        ads:adUnitId="@string/banner_ad_unit_id"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        android:layout_alignParentBottom="true"
+        android:layout_centerHorizontal="true"></com.google.android.gms.ads.AdView>
+
+    <com.google.android.gms.ads.AdView
+        android:id="@+id/llv"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        android:layout_alignParentBottom="true"
+        android:layout_centerHorizontal="true"
+        android:layout_marginBottom="8dp"
+        ads:adSize="BANNER"
+        ads:adUnitId="@string/banner_ad_unit_id"
+        ads:layout_constraintBottom_toBottomOf="parent"></com.google.android.gms.ads.AdView>
+ */
+
+public class Load extends AppCompatActivity {
+
+    // Setup WIFI
+    WifiManager wifimanager;
+    private IntentFilter mFilter;
+
+    private int ck=0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.load);
+
+        //Admob
+       /* AdView mAdView = (AdView) findViewById(R.id.lv);
+        AdRequest adRequest = new AdRequest.Builder().addTestDevice(AdRequest.DEVICE_ID_EMULATOR).build();
+        //AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+        AdView mmAdView = (AdView) findViewById(R.id.llv);
+        AdRequest aadRequest = new AdRequest.Builder().addTestDevice(AdRequest.DEVICE_ID_EMULATOR).build();
+        //AdRequest aadRequest = new AdRequest.Builder().build();
+        mmAdView.loadAd(aadRequest);*/
+
+        wifimanager= (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
+
+        // WIFI ON
+        if (wifimanager.isWifiEnabled() == false)
+            wifimanager.setWifiEnabled(true);
+
+        //Setup check real-time WiFi status
+        mFilter = new IntentFilter(WifiManager.WIFI_STATE_CHANGED_ACTION);
+        mFilter.addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION);
+        mFilter.addAction(WifiManager.RSSI_CHANGED_ACTION);
+        registerReceiver(mReceiver, mFilter);
+
+        //Loading Window
+        CheckTypesTask task = new CheckTypesTask();
+        task.execute();
+    }
+
+    private class CheckTypesTask extends AsyncTask<Void, Void, Void> {
+
+        ProgressDialog asyncDialog = new ProgressDialog(
+                Load.this);
+
+        //View Loading Window
+        @Override
+        protected void onPreExecute() {
+            asyncDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            asyncDialog.setMessage("WIFI is connecting now");
+
+            asyncDialog.setCancelable(false);
+            // show dialog
+            asyncDialog.show();
+            super.onPreExecute();
+        }
+
+        //Runnig Loading Window Maximum 30sec
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            try {
+                for(int i=0;i<30; i++) {
+                    if(ck==1)
+                        break;
+                    else {
+                        Thread.sleep(1000);
+                        initWIFIScan();
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        //Close Loading Window
+        @Override
+        protected void onPostExecute(Void result) {
+            asyncDialog.dismiss();
+            super.onPostExecute(result);
+
+            show();
+        }
+    }
+
+    //Surrounding area wifi scan
+    private List<ScanResult> mScanResult; // ScanResult List
+    private BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        NetworkInfo nifo;
+        //Get wifi lists.
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+            if (action.equals(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)) {
+                getWIFIScanResult(); // get WIFISCanResult
+                wifimanager.startScan(); // for refresh
+            } else if (action.equals(WifiManager.NETWORK_STATE_CHANGED_ACTION)) {
+                sendBroadcast(new Intent("wifi.ON_NETWORK_STATE_CHANGED"));
+            }
+
+            //Check real-time WiFi status
+            if (action.equals(WifiManager.NETWORK_STATE_CHANGED_ACTION)) {
+                nifo = (NetworkInfo)intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
+                //Wifi is on
+                if (nifo.getState().equals(NetworkInfo.State.CONNECTED)) {
+                    ck=1;
+                    unregisterReceiver(mReceiver);
+                }
+            }
+        }
+    };
+    ArrayList<String> li = new ArrayList<String>();
+    public void getWIFIScanResult() {
+        mScanResult = wifimanager.getScanResults(); // ScanResult
+        // Scan count
+        for (int i = 0; i < mScanResult.size(); i++) {
+            ScanResult result = mScanResult.get(i);
+            String Capabilities =  result.capabilities;
+
+            /*if(result.SSID.toString().matches(".*KT.*") || result.SSID.toString().matches(".*olleh.*") || result.SSID.toString().matches(".*U+.*")
+                    || result.SSID.toString().matches(".*SK.*") || result.SSID.toString().matches("T"))
+                    continue;*/
+
+            //Blocking Free carrier WIFI
+            if(Capabilities.contains("EAP")) {
+                continue;
+            }
+            //Automatic connection
+            else {
+                li.add(result.SSID.toString());
+                WifiConfiguration wificonfig = new WifiConfiguration();
+                wificonfig.SSID = String.format("\"%s\"", result.SSID.toString());
+                wificonfig.preSharedKey = String.format("\"%s\"", RT);
+                int netId = wifimanager.addNetwork(wificonfig);
+                wifimanager.disconnect();
+                wifimanager.enableNetwork(netId,false);
+                wifimanager.reconnect();
+
+                //One ap give 10sec
+                try{
+                    Thread.sleep(10000);
+                }catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                //Check WIFI stauts
+                ConnectivityManager manager =(ConnectivityManager)this.getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo WIFI = manager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+                if(WIFI.isConnected()){
+                    break;
+                }
+                else{
+                    wifimanager.removeNetwork(netId);
+                    wifimanager.saveConfiguration();
+                }
+            }
+        }
+        unregisterReceiver(mReceiver); // stop WIFISCan
+    }
+
+    public void initWIFIScan() {
+        // init WIFISCAN
+        final IntentFilter filter = new IntentFilter(
+                WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+        filter.addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION);
+        registerReceiver(mReceiver, filter);
+        wifimanager.startScan();
+    }
+
+    private void show(){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        //If WIFI connected right
+        if(ck==1) {
+            //Toast.makeText(getApplicationContext(),"연결 성공!",Toast.LENGTH_LONG).show();
+                builder.setTitle("WIFI connection successful!");
+                builder.setCancelable(false);
+                builder.setPositiveButton("END",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                ActivityCompat.finishAffinity(Load.this);
+                            }
+                        });
+                builder.setNegativeButton("Go First",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                            }
+                        });
+                builder.show();
+        }
+        //Not connected wifi
+        else{
+           // Toast.makeText(getApplicationContext(),"연결 실패...",Toast.LENGTH_LONG).show();
+                builder.setTitle("WIFI connection failed...\n(Temporary connection fail or password error!)");
+                builder.setCancelable(false);
+                builder.setPositiveButton("END",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                ActivityCompat.finishAffinity(Load.this);
+                            }
+                        });
+                builder.setNegativeButton("Go First",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                            }
+                        });
+                builder.show();
+        }
+    }
+
+    //Stop Backpress
+    @Override
+    public void onBackPressed() {
+        return;
+    }
+
+}
---
+++ SnapWIFI/app/src/main/java/com/example/unknown/snapwifi/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.unknown.snapwifi;
+
+import android.Manifest;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Clear the status bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
+                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        //Use TedPermission
+        PermissionListener permissionlistener = new PermissionListener() {
+            @Override
+            public void onPermissionGranted() {
+                startActivity(new Intent(MainActivity.this,Cam.class));
+            }
+
+            @Override
+            public void onPermissionDenied(ArrayList<String> deniedPermissions) {
+                Intent intent = getIntent();
+                finish();
+                startActivity(intent);
+            }
+        };
+        TedPermission.with(this)
+                .setPermissionListener(permissionlistener)
+                //.setRationaleMessage("어플을 이용하기 위해선 접근 권한이 필요합니다")
+                .setDeniedMessage("You have declined ... but you can grant permissions under Settings> Permissions")
+                .setPermissions(Manifest.permission.CAMERA, Manifest.permission.ACCESS_COARSE_LOCATION)
+                .check();
+    }
+}
+
